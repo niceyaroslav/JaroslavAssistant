59abaaad5eed722623f1a6e1da9ee5be2ff675eb
Jaroslav Marhivka <jaroslav@tftak.eu>
2021-06-28 17:49:58 +0300

    changed structure and dependecies

--- bot.py
+++ bot.py
@@ -1,18 +1,63 @@
-import telebot
-bot = telebot.TeleBot('964603618:AAF2JBJnsWqho3fFoY9tndxkRFjJKBPMILM')
+import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
-@bot.message_handler(commands=['start'])
-def test(message):
-    bot.reply_to(message, f'Hi, I am bot of Jaroslav. Nice to meet you {message.from_user.first_name}')
+TOKEN = '964603618:AAF2JBJnsWqho3fFoY9tndxkRFjJKBPMILM'
+PORT = int(os.environ.get('PORT', 5000))
 
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.from_user.id, 'Привет!')
-    else:
-        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
 
 
-bot.polling(none_stop=True)
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+# def error(update, context):
+#     """Log Errors caused by Updates."""
+#     logger.warning('Update "%s" caused error "%s"', update, context.error)
+#
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    # dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://jaroslav-bot.herokuapp.com/' + TOKEN)
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
+
