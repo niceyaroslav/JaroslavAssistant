1ef37c695cf757a0d9999e6fe2cb7e354bf56af3
Jaroslav Marhivka <jaroslav@tftak.eu>
2021-06-28 16:14:04 +0300

    added files for heroku deployment

--- bot.py
+++ bot.py
@@ -6,4 +6,13 @@ bot = telebot.TeleBot('964603618:AAF2JBJnsWqho3fFoY9tndxkRFjJKBPMILM')
 def test(message):
     bot.reply_to(message, f'Hi, I am bot of Jaroslav. Nice to meet you {message.from_user.first_name}')
 
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.from_user.id, 'Привет!')
+    else:
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+
+
 bot.polling(none_stop=True)
