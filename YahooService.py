396d1b70045c937dd5c458ef3bde140efb5d3a7c
Jaroslav Marhivka <jaroslav@tftak.eu>
2021-06-30 00:55:20 +0300

    now can fetch prices

--- YahooService.py
+++ YahooService.py
@@ -1,24 +1,38 @@
 import yfinance as yf
 import datetime
 
+
 class YahooService:
 
     coin_dict = {
-        "Ether": "",
-        "Bitcoin": "",
-        "Cardano": "",
-        "Polkadot": "",
-        "Stellar": "",
-        "Ripple": "",
-        "Tezos": "",
-        "Litecoin": ""
+        "Ether": "ETH",
+        "Bitcoin": "BTC",
+        "Cardano": "ADA",
+        "Polkadot": "DOT1",
+        "Stellar": "XLM",
+        "Ripple": "XRP",
+        "Litecoin": "LTC",
+        "Doge": "DOGE"
     }
+
+    def coin_dict_to_text(self):
+        coins = ""
+        for k, v in self.coin_dict.items():
+            coins += f"{k} - {v}\n"
+        return coins
+
+    def generate_coin_id(self, coin_name, currency):
+        proper_name = coin_name.capitalize()
+        return f"{self.coin_dict[proper_name]}-{currency}"
 
     @staticmethod
     def get_data(token):
         ticker = yf.Ticker(token)
-        data = ticker.history(period="max")
-        return data
+        now = datetime.datetime.now().date().isoformat()
+        data = ticker.history(start=now, stop=now, interval="15m")
+        return data["Close"][-1]
 
-kk = YahooService()
-a = kk.get_data('XLM-EUR')
+
+if __name__ == '__main__':
+    kk = YahooService()
+    a = kk.get_data('XLM-USD')
--- bot.py
+++ bot.py
@@ -1,25 +1,46 @@
-import os
-import dotenv
+from Constants import API_KEY
+from YahooService import YahooService as ys
 import telebot
 
-dotenv.load_dotenv()
-# PORT = int(os.environ.get('PORT', 8443))
-TOKEN = os.getenv('TOKEN')
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(API_KEY)
+service = ys()
 
 
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(chat_id=message.chat.id, text="""\
+Hey buddy, I am CryptoPeaceDuke (read Crypto Pizdyuk, blyat).
+Here you can ask me about the current prices for certain crypto coins. I will also periodically notify you If Stellar 
+goes high in price, since my creator wants to ditch that shit real quick :)
+
+To get a list of coins type "/coins".\
+""")
+
+
+@bot.message_handler(commands=['coins'])
+def get_coin_list(message):
+    bot.send_message(chat_id=message.chat.id, text=f"""\
+Hey buddy! Here is the list of coins I can tell you price for:
+{service.coin_dict_to_text()}
+To get price of desired coin type coin name, currency like so -> Ether, USD"\
 """)
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
+# @bot.message_handler(func=lambda message: True)
+# def echo_message(message):
+#     bot.reply_to(message, message.text)
+
+
+@bot.message_handler(regexp=r"[a-zA-Z]+,\s[a-zA-Z]{3}", content_types=["text"])
+def get_price(message):
+    words = message.text.split(", ")
+    if words[0].capitalize() in ys.coin_dict.keys():
+        token = "-".join([ys.coin_dict[words[0]], words[1].upper()])
+        price = round(service.get_data(token), 4)
+        bot.send_message(chat_id=message.chat.id, text=f"Current price for this coin is {price} {words[1]}")
+    else:
+        bot.send_message(chat_id=message.chat.id, text=f"Can't find data on this coin :(")
 
 
 bot.polling(none_stop=True)
