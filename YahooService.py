e61fc9e48d75f9aa695910702329492b4a106490
Jaroslav Marhivka <jaroslav@tftak.eu>
2021-06-29 17:55:58 +0300

    updated, now working

--- YahooService.py
+++ YahooService.py
@@ -1,4 +1,24 @@
 import yfinance as yf
+import datetime
 
 class YahooService:
-    pass
+
+    coin_dict = {
+        "Ether": "",
+        "Bitcoin": "",
+        "Cardano": "",
+        "Polkadot": "",
+        "Stellar": "",
+        "Ripple": "",
+        "Tezos": "",
+        "Litecoin": ""
+    }
+
+    @staticmethod
+    def get_data(token):
+        ticker = yf.Ticker(token)
+        data = ticker.history(period="max")
+        return data
+
+kk = YahooService()
+a = kk.get_data('XLM-EUR')
