ed90eeabc1bc1533d122b6e00cf49ab3c00f67c7
Jaroslav Marhivka <jaroslav@tftak.eu>
2021-06-30 12:33:58 +0300

    updated code, trying to deploy

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 from Constants import API_KEY
 from YahooService import YahooService as ys
 import telebot
-
+# from flask import Flask, request
+# import os
 
 bot = telebot.TeleBot(API_KEY)
 service = ys()
+# server = Flask(__name__)
 
 
 @bot.message_handler(commands=['start'])
@@ -23,7 +25,7 @@ def get_coin_list(message):
     bot.send_message(chat_id=message.chat.id, text=f"""\
 Hey buddy! Here is the list of coins I can tell you price for:
 {service.coin_dict_to_text()}
-To get price of desired coin type coin name, currency like so -> Ether, USD"\
+To get price of desired coin type coin "name, currency" like so -> Ether, USD\
 """)
 
 
@@ -36,13 +38,32 @@ To get price of desired coin type coin name, currency like so -> Ether, USD"\
 @bot.message_handler(regexp=r"[a-zA-Z]+,\s[a-zA-Z]{3}", content_types=["text"])
 def get_price(message):
     words = message.text.split(", ")
-    if words[0].capitalize() in ys.coin_dict.keys():
-        token = "-".join([ys.coin_dict[words[0]], words[1].upper()])
+    coin = words[0].capitalize()
+    currency = words[1].upper()
+    if coin in ys.coin_dict.keys():
+        token = "-".join([ys.coin_dict[coin], currency])
         price = round(service.get_data(token), 4)
-        bot.send_message(chat_id=message.chat.id, text=f"Current price for this coin is {price} {words[1]}")
+        bot.send_message(chat_id=message.chat.id, text=f"Current price for this coin is {price} {currency}")
     else:
-        bot.send_message(chat_id=message.chat.id, text=f"Can't find data on this coin :(")
+        bot.send_message(chat_id=message.chat.id, text=f"Can't find data for {coin} :(")
 
+# @server.route('/' + API_KEY, methods=['POST'])
+# def getMessage():
+#     json_string = request.get_data().decode('utf-8')
+#     update = telebot.types.Update.de_json(json_string)
+#     bot.process_new_updates([update])
+#     return "!", 200
+#
+#
+# @server.route("/")
+# def webhook():
+#     bot.remove_webhook()
+#     bot.set_webhook(url='https://your_heroku_project.com/' + API_KEY)
+#     return "!", 200
 
 bot.polling(none_stop=True)
 
+#
+# if __name__ == "__main__":
+#     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+#
